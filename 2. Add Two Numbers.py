21b9808db31b8b740a8bc9072d19a995ff48121f
HarleyHu <15033157073@163.com>
2018-05-15 16:28:15 +0800

    Update 2. Add Two Numbers.py

--- 2. Add Two Numbers.py
+++ 2. Add Two Numbers.py
@@ -11,54 +11,33 @@ class Solution:
         :type l2: ListNode
         :rtype: ListNode
         """
-        res1 = l1
-        res2 = l2
-        len1 = 0
-        len2 = 0
-        last1 = l1
-        last2 = l2
-        while l1 != None or l2 != None :
-            if l1 == None :
-                if l2.val > 9:
-                    l2.val = l2.val%10
-                    if l2.next == None :
-                        last2.next = ListNode(1)
-                        len2 += 1
-                    else :
-                        l2.next.val += 1
-                len2 += 1
-                if l2.next != None :
-                    last2 = l2.next
+        p = ListNode(0)
+        res = p
+        carry = 0
+        while l1 or l2 or carry:
+            if l1 == None and l2 :
+                sum = l2.val + carry
+                p.next = ListNode(sum%10)
+                p = p.next
                 l2 = l2.next
-            elif l2 == None :
-                if l1.val > 9:
-                    l1.val = l1.val%10
-                    if l1.next == None :
-                        last1.next = ListNode(1)
-                    else :
-                        l1.next.val += 1
-                len1 += 1
-                if l1.next != None :
-                    last1 = l1.next
+                carry = sum//10
+                continue
+            if l1 and l2 == None :
+                sum = l1.val + carry
+                p.next = ListNode(sum%10)
+                p = p.next
                 l1 = l1.next
-            else :
-                tmp = l1.val + l2.val
-                l1.val = tmp%10
-                l2.val = tmp%10
-                if tmp > 9 :
-                    if l1.next != None :
-                        l1.next.val += 1
-                    else :
-                        last1.next = ListNode(1)
-                len1 += 1
-                len2 += 1
-                if l1.next != None :
-                    last1 = l1.next
-                if l2.next != None :
-                    last2 = l2.next
-                l1 = l1.next
-                l2 = l2.next
-        if len2 > len1 :
-            return res2
-        else :
-            return res1
+                carry = sum//10
+                continue
+            if l1 == None and l2 == None :
+                p.next = ListNode(carry)
+                p = p.next
+                carry = 0
+                continue
+            sum = l1.val + l2.val + carry
+            p.next = ListNode(sum%10)
+            carry = sum//10
+            p = p.next
+            l1 = l1.next
+            l2 = l2.next
+        return res.next
