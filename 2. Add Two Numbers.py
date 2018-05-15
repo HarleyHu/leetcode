1f8bc5d71c663be953c07d83f14e5f9461576f9e
HarleyHu <15033157073@163.com>
2018-05-15 15:56:45 +0800

    Update 2. Add Two Numbers.py

--- 2. Add Two Numbers.py
+++ 2. Add Two Numbers.py
@@ -11,31 +11,54 @@ class Solution:
         :type l2: ListNode
         :rtype: ListNode
         """
-        lr = ListNode()
-        ltmp = lr
-        if l1 == None || l2 == None:
-            return flase
-        while true: 
-            ltmp.val = l1.val + l2.val
-            ltmp.next = ListNode() 
-            ltmp = ltmp.next
-            l1 = l1.next
-            l2 = l2.next
-            if l1 == None:
-                ltmp = l2
-                break
-            if l2 == None:
-                ltmp = l1
-                break
-        
-        ltmp1 = ltmp
-        ltmp2 = ltmp
-        while ltmp.next != None:
-            ltmp1 = ltmp2
-            while ltmp1.next != None:
-                ltmp1 = ltmp1.next 
-            ltmp.val = ltmp1.val
-            ltmp = ltmp.next
-            ltmp1 = None
-            ltmp = ltmp.next
-        return ltmp
+        res1 = l1
+        res2 = l2
+        len1 = 0
+        len2 = 0
+        last1 = l1
+        last2 = l2
+        while l1 != None or l2 != None :
+            if l1 == None :
+                if l2.val > 9:
+                    l2.val = l2.val%10
+                    if l2.next == None :
+                        last2.next = ListNode(1)
+                        len2 += 1
+                    else :
+                        l2.next.val += 1
+                len2 += 1
+                if l2.next != None :
+                    last2 = l2.next
+                l2 = l2.next
+            elif l2 == None :
+                if l1.val > 9:
+                    l1.val = l1.val%10
+                    if l1.next == None :
+                        last1.next = ListNode(1)
+                    else :
+                        l1.next.val += 1
+                len1 += 1
+                if l1.next != None :
+                    last1 = l1.next
+                l1 = l1.next
+            else :
+                tmp = l1.val + l2.val
+                l1.val = tmp%10
+                l2.val = tmp%10
+                if tmp > 9 :
+                    if l1.next != None :
+                        l1.next.val += 1
+                    else :
+                        last1.next = ListNode(1)
+                len1 += 1
+                len2 += 1
+                if l1.next != None :
+                    last1 = l1.next
+                if l2.next != None :
+                    last2 = l2.next
+                l1 = l1.next
+                l2 = l2.next
+        if len2 > len1 :
+            return res2
+        else :
+            return res1
