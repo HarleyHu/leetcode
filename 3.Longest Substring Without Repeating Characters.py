be4f8fbd74b5ea3a7a6851ccdcfc306c9173eac8
HarleyHu <15033157073@163.com>
2018-05-23 14:57:24 +0800

    Update 3.Longest Substring Without Repeating Characters.py

--- 3.Longest Substring Without Repeating Characters.py
+++ 3.Longest Substring Without Repeating Characters.py
@@ -7,16 +7,20 @@ class Solution:
         if s == "" :
             return 0
         high = len(s) - 1
-        low = high - 1
+        low = high
         res = 1
+        hashTable = {}
         while low >= 0 :
-            if s[low] in s[low+1:high+1] :
+            if s[low] in hashTable :
+                tmp = hashTable[s[low]]
                 h = high - low
-                if h > res :
-                    res = h
-                high = s[low+1:high+1].index(s[low]) + low
-                low = high - 1
+                if h > res : res = h
+                hashTable.clear()
+                high = tmp - 1
+                low = high -1
+                hashTable[s[high]] = high
             else :
+                hashTable[s[low]] = low
                 low -= 1
         h = high - low
         if h > res :
